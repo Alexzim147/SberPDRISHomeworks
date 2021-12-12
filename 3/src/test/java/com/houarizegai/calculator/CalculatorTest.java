1c01db9e0a873af7c14bc18772527465b8ece84d
alexzim147 <zimenkov.shura@mail.ru>
2021-12-12 19:39:33 +0300

    pom.xml changed

--- 3/src/test/java/com/houarizegai/calculator/CalculatorTest.java
+++ 3/src/test/java/com/houarizegai/calculator/CalculatorTest.java
@@ -1,8 +1,8 @@
 package com.houarizegai.calculator;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 class CalculatorTest {
 
@@ -22,12 +22,12 @@ class CalculatorTest {
         String second = "5";
 
         try {
-            Assertions.assertEquals(8, calculator.calc(first, second, '+'));
-            Assertions.assertEquals(-2, calculator.calc(first, second, '-'));
-            Assertions.assertEquals(15, calculator.calc(first, second, '*'));
-            Assertions.assertEquals(0.6, calculator.calc(first, second, '/'));
-            Assertions.assertEquals(3d, calculator.calc(first, second, '%'));
-            Assertions.assertEquals(243, calculator.calc(first, second, '^'));
+            assertEquals(8, calculator.calc(first, second, '+'));
+            assertEquals(-2, calculator.calc(first, second, '-'));
+            assertEquals(15, calculator.calc(first, second, '*'));
+            assertEquals(0.6, calculator.calc(first, second, '/'));
+            assertEquals(3d, calculator.calc(first, second, '%'));
+            assertEquals(243, calculator.calc(first, second, '^'));
         } catch (Exception e) {
             e.printStackTrace();
         }
